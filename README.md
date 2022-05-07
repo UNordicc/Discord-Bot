1dcb4d5881a70c78aa3594898e490d99e11795ff
UNordic <unordic@vivaldi.net>
2022-05-07 12:49:00 +0200

    master

---
+++ README.md
@@ -0,0 +1,2 @@
+## UNordic Discord Bot
+Dumb ass trying to make own Discord Bot
---
+++ commands/ping.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Pong')
+		.addUserOption(option => option.setName('target').setDescription('The member to kick')),
+	async execute(interaction) {
+		const user = interaction.options.getUser('target');
+		return interaction.reply({ content: `Pong!`, ephemeral: true });
+	},
+};
